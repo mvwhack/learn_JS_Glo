c80be09bd5012154500a6409e9482174da31670e
Vlad <vmansurov@icloud.com>
2020-10-09 12:55:53 +0300

    Начал выполнение второго домашнего задания

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 150000;
+console.log(typeof money);
+
+let income = 'Разработка сайтов на фрилансе';
+console.log(typeof income);
+
+let addExpenses = 'Квартплата, Интернет, Телефоны, Бензин';
+
+let deposit = true;
+console.log(typeof deposit);
+
+let mission = 1000000;
+
+let period = 12;
 
 
 alert('Привет, меня зовут Влад');
