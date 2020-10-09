c7935f50a73e76ea489c0e38adf90c6d50be7f02
Vlad <vmansurov@icloud.com>
2020-10-09 14:02:10 +0300

    Выполнил основное заданиае 2 урока

--- js/main.js
+++ js/main.js
@@ -5,15 +5,22 @@ let income = 'Разработка сайтов на фрилансе';
 console.log(typeof income);
 
 let addExpenses = 'Квартплата, Интернет, Телефоны, Бензин';
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase().split(', '));
 
 let deposit = true;
 console.log(typeof deposit);
 
 let mission = 1000000;
+console.log('Цель заработать ' + mission + ' рублей');
 
 let period = 12;
+console.log('Период равен ' + period + ' месяцев');
 
 
 alert('Привет, меня зовут Влад');
 
 console.log('Проверка работы консоли');
+
+let budgetDay = 150000 / 30;
+console.log(budgetDay);
